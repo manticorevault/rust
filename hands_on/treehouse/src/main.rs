84577355c33f167c032c54ee2af6cb8d1d8db05e
Artur Serra <artur.serra@qik.ro>
2022-06-30 17:00:57 +0400

    Added a new iterator and refactored the visitor list code

--- hands_on/treehouse/src/main.rs
+++ hands_on/treehouse/src/main.rs
@@ -1,9 +1,28 @@
 // Import the input from std library
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
+// Adding the visitor struct
+struct Visitor {
+    name: String,
+    greeting: String
+}
 
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!(
+            "{}", self.greeting
+        )
+    }
+}
+
+fn name_catcher() -> String {
     // Store the user's name as a mut variable
     let mut your_name = String::new();
 
@@ -11,6 +30,30 @@ fn main() {
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
+    
+    // Returns the name
+    your_name
+        .trim()
+        .to_lowercase()
+}
 
-    println!("Hello, {}", your_name);
+
+fn main() {
+    println!("Hello, what's your name?");
+    let name = name_catcher();
+    let visitor_list = [
+        Visitor::new("artur", "Ola, Artur!"),
+        Visitor::new("masha", "Ola, Masha! Tu ta gostosa!"),
+        Visitor::new("josephine", "Ola, Josephine!"),
+    ];
+
+    let known_visitors = visitor_list
+                            .iter()
+                            .find(| visitor|
+                                    visitor.name == name);
+
+    match known_visitors {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Sorry, your name is not on the list")
+    }
 }
